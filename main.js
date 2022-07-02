495b95024956f0017202057e1d081b5a144d7b2d
Nichita Gabriel-Ionut <gabinichita1999@gmail.com>
2022-07-02 16:50:13 +0300

    added functionality for salary sum function

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 function calculateNetSalary() {
-    let grossSalary = document.getElementById("gross-salary").value;
-    let netSalary = (45 / 100) * grossSalary;
-  
-    if (grossSalary != null) {
-      document.getElementById("display-gross").innerHTML =
-        "Gross Salary: " + grossSalary;
-    }
-  
-    if (netSalary != null) {
-      document.getElementById("display-net").innerHTML =
-        "Net Salary: " + netSalary;
-    }
+  let grossSalary = document.getElementById("gross-salary").value;
+  let netSalary = (45 / 100) * grossSalary;
+
+  if (grossSalary) {
+    document.getElementById("display-gross").innerHTML =
+      "Gross Salary: " + grossSalary;
   }
 
-function sumSalaries() {}
+  if (netSalary) {
+    document.getElementById("display-net").innerHTML =
+      "Net Salary: " + netSalary.toFixed();
+  }
+}
+
+function sumSalaries() {
+  let firstSalary = parseInt(document.getElementById("salary-1").value);
+  let secondSalary = parseInt(document.getElementById("salary-2").value);
+  let thirdSalary = parseInt(document.getElementById("salary-3").value);
+  let fourthSalary = parseInt(document.getElementById("salary-4").value);
+  let fifthSalary = parseInt(document.getElementById("salary-5").value);
+
+  let additionSalaries =
+    firstSalary + secondSalary + thirdSalary + fourthSalary + fifthSalary;
+
+  if (additionSalaries) {
+    document.getElementById("display-salary-sum").innerHTML =
+      "Sum of salaries is: " + additionSalaries;
+  }
+}
 
 function sumSubsetSalaries() {}
 
