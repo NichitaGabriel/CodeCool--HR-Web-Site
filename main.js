b1f0b933b13cb90aaadf6b77d09574de32019629
Nichita Gabriel-Ionut <gabinichita1999@gmail.com>
2022-07-02 15:14:58 +0300

    added functionality for salary calculator

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-function calculateNetSalary() {}
+function calculateNetSalary() {
+    let grossSalary = document.getElementById("gross-salary").value;
+    let netSalary = (45 / 100) * grossSalary;
+  
+    if (grossSalary != null) {
+      document.getElementById("display-gross").innerHTML =
+        "Gross Salary: " + grossSalary;
+    }
+  
+    if (netSalary != null) {
+      document.getElementById("display-net").innerHTML =
+        "Net Salary: " + netSalary;
+    }
+  }
 
 function sumSalaries() {}
 
